206b23ca668f8b367c76af77f1ec1590c84ced1c
Stephen Paladines <dragon9man@ufl.edu>
2019-09-25 17:12:09 -0400

    Hooks Update
    -Added hooks to existing meme generator (useState)

--- src/memeGenerator/Components/Image-Component.jsx
+++ src/memeGenerator/Components/Image-Component.jsx
@@ -1,39 +1,36 @@
 import React from "react";
 
-class ImageComponent extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="test">
-          <div className="inputs">
-            <input
-              type="text"
-              placeholder={"Top Text"}
-              value={this.props.image.upperText}
-              name={"upperText"}
-              onChange={this.props.image.functions.handleChange}
-            />
-            <input
-              type="text"
-              placeholder={"Lower Text"}
-              value={this.props.image.lowerText}
-              name={"lowerText"}
-              onChange={this.props.image.functions.handleChange}
-            />
-            <button onClick={this.props.image.functions.generateImage}>
-              Generate
-            </button>
-          </div>
-          <br />
-          <div className="img-container">
-            <img src={this.props.image.imgSrc} alt="" id="#image" />
-            <p id="upperText">{this.props.image.upperText}</p>
-            <p id="lowerText">{this.props.image.lowerText}</p>
-          </div>
+const ImageComponent = props => {
+  return (
+    <div className="container">
+      <div className="test">
+        <div className="inputs">
+          <input
+            type="text"
+            placeholder={"Top Text"}
+            value={props.upperText}
+            name={"upperText"}
+            onChange={props.handleChange}
+          />
+          <input
+            type="text"
+            placeholder={"Lower Text"}
+            value={props.lowerText}
+            name={"lowerText"}
+            onChange={props.handleChange}
+          />
+          <button onClick={props.generateImage}>Generate</button>
+        </div>
+        <br />
+        <div className="img-container">
+          <img src={props.imageSrc} alt="" id="#image" />
+          {console.log("Testing... " + props.imageSrc)}
+          <p id="upperText">{props.upperText}</p>
+          <p id="lowerText">{props.lowerText}</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ImageComponent;
--- src/memeGenerator/Container/Image-Container.jsx
+++ src/memeGenerator/Container/Image-Container.jsx
@@ -1,46 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageComponent from "../Components/Image-Component";
 
-class ImageContainer extends React.Component {
-  constructor() {
-    super();
-    // No longer needed w/ arrrow function
-    // this.generateImage = this.generateImage.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      upperText: "",
-      lowerText: "",
-      imgSrc: "",
-      functions: {
-        handleChange: this.handleChange,
-        generateImage: this.generateImage
-      }
-    };
-  }
-  componentDidMount() {
-    this.generateImage();
-  }
+const ImageContainer = () => {
+  const [{ upperText, lowerText }, setInputText] = useState({
+    upperText: "",
+    lowerText: ""
+  });
 
-  generateImage = () => {
+  const [imageSrc, setImageSrc] = useState("http://lorempixel.com/400/400/");
+
+  const generateImage = () => {
     fetch("https://picsum.photos/400").then(response => {
-      console.log(response.url);
-      this.setState({ imgSrc: response.url });
+      return setImageSrc(response.url);
     });
   };
 
-  handleChange = event => {
-    console.log(event.target);
+  const handleChange = event => {
+    // console.log(event.target);
     // Object destructuring
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Array_destructuring
     const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+    return setInputText(prevState => ({
+      ...prevState, // Preserves the keys of the state
+      [name]: value // Updates the corresponding state with the value
+    }));
   };
 
-  render() {
-    return <ImageComponent image={this.state} />;
-  }
-}
+  return (
+    <ImageComponent
+      imageSrc={imageSrc}
+      upperText={upperText}
+      lowerText={lowerText}
+      handleChange={handleChange}
+      generateImage={generateImage}
+    />
+  );
+};
 
 export default ImageContainer;
